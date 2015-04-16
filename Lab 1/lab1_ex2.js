7fccbce2df96f209af3ca2b9788ae20bb7ebbeb2
MKWhit <MorganWhitlow@gmail.com>
2015-04-16 14:21:28 -0400

    Lab 1
    Lab 1A.  calculates age in dog years
    Lab 1B. An exercise in changing background via JavaScript
    Lab 1C. Using JS to make a list of favorite things

---
+++ Lab 1/lab1_ex2.js
@@ -0,0 +1,4 @@
+document.write("The title of this page is the some-what deranged "+document.title+"<br>"+"<br>");
+document.write("The URL, "+document.URL+", is only a local path and useless outside of this particular machine"+"<br>"+"<br>");
+  
+document.write("The background color is a soothing \(if tacky\) "+document.body.style.backgroundColor+"<br>"+"<br>");
